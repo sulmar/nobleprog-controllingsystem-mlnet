f2793587f44ed2378a183fd861c8a6337ed7ef62
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-09-27 15:35:53 +0200

    Dodanie forecasting

---
+++ src/Forecasting/ProductSalesForecasting/ProductSaleData.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductSalesForecasting;
+
+public class ProductSaleData
+{
+    public string product { get; set; }
+    public float customerid { get; set; }
+    public DateTime orderdate { get; set; }
+    public float regionid { get; set; }
+    public float quantity { get; set; }
+}
