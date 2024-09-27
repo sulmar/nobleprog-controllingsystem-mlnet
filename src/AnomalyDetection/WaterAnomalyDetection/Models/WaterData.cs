f28b168178f3926161bb5952d5ca6eb91b3a2fcd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-09-27 11:22:29 +0200

    Dodanie wykrywania anomali za pomoca kmeans

---
+++ src/AnomalyDetection/WaterAnomalyDetection/Models/WaterData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.ML.Data;
+
+namespace WaterAnomalyDetection.Models;
+
+public class WaterData
+{
+    [LoadColumn(0)]
+    public float ODBIORCA_ID { get; set; }
+
+    [LoadColumn(1)]
+    public float OBSZAR_ID { get; set; }
+
+    [LoadColumn(2)]
+    public float ZRODLO_ID { get; set; }
+
+    [LoadColumn(3)]
+    public float SEZON_ID { get; set; }
+
+    [LoadColumn(4)]
+    public float OPIS_ID { get; set; }
+
+    [LoadColumn(5)]
+    public float POBOR { get; set; }
+
+    [LoadColumn(6)]
+    public float CZAS_ID { get; set; }
+
+    [LoadColumn(7)]
+    public string Przyczyna { get; set; }
+} 
+
+public class WaterPrediction
+{
+    public uint PredictedLabel { get; set; }
+    public float[] Score { get; set; }
+}
---
+++ src/AnomalyDetection/WaterAnomalyDetection/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Trainers;
+using WaterAnomalyDetection.Models;
+
+Console.WriteLine("Hello, World!");
+
+
+var dataPath = Path.Combine("Data", "water-data.csv");
+
+// 1. Utworzenie nowego kontekstu ML.NET
+var context = new MLContext();
+
+// 2. Wczytanie danych z pliku CSV
+IDataView data = context.Data.LoadFromTextFile<WaterData>(dataPath, separatorChar: ',', hasHeader: true);
+
+// 3. Utwórz trenera
+var options = new KMeansTrainer.Options { InitializationAlgorithm = KMeansTrainer.InitializationAlgorithm.KMeansPlusPlus, NumberOfClusters = 3 };
+
+var trainer = context.Clustering.Trainers.KMeans(options);
+
+// 4. Utwórz pipeline
+var pipeline =
+      context.Transforms.Concatenate("Features", "ODBIORCA_ID", "OBSZAR_ID", "ZRODLO_ID", "SEZON_ID", "POBOR", "CZAS_ID")
+   // context.Transforms.Concatenate("Features", "ODBIORCA_ID", "POBOR")
+    .Append(trainer);
+
+var preview = data.Preview();
+
+// 5. Trenujemy
+var model = pipeline.Fit(data);
+
+// 6. Uzyjemy modelu do predykcji
+var predictions = model.Transform(data);
+
+var predictionsResults = context.Data.CreateEnumerable<WaterPrediction>(predictions, reuseRowObject: false);
+
+foreach (var prediction in predictionsResults)
+{
+    Console.WriteLine($"Predykowany klaster: {prediction.PredictedLabel}, Wynik: {string.Join(", ", prediction.Score)}");
+
+    for(int i = 0; i < prediction.Score.Length; i++)
+    {
+        Console.WriteLine($"Odległość od centroidu {i}: {prediction.Score[i]}");
+    }
+}
+
+
+
