663c4a5699e0d30ca841f192ded4b8177b3b9c05
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-09-27 09:40:47 +0200

    Zapisywanie i ladowanie modelu z dysku

--- src/Regression/PredicateSalary/Program.cs
+++ src/Regression/PredicateSalary/Program.cs
@@ -4,58 +4,81 @@ using PredicateSalary.Models;
 
 Console.WriteLine("Hello, World!");
 
-var dataPath = Path.Combine("Data", "salary-data.csv");
+// TrainAndSaveModel();
 
-// dotnet add package Microsoft.ML
+for(float yearsExperience = 1.0f ; yearsExperience <= 65; yearsExperience = yearsExperience + 0.5F)
+{ 
+    UseTrainedModel(yearsExperience);
 
-// 1. Utworzenie nowego kontekstu ML.NET
-MLContext context = new MLContext(seed: 1);
+   // Thread.Sleep(1000);
+}
 
-// 2. Wczytanie danych z pliku CSV
-IDataView dataView = context.Data.LoadFromTextFile<SalaryData>(dataPath, separatorChar: ',', hasHeader: true);
+static void TrainAndSaveModel()
+{
+    var dataPath = Path.Combine("Data", "salary-data.csv");
 
-var dataSplit = context.Data.TrainTestSplit(dataView, testFraction: 0.2);
-var trainingData = dataSplit.TrainSet;
-var testData = dataSplit.TestSet;
+    // dotnet add package Microsoft.ML
 
+    // 1. Utworzenie nowego kontekstu ML.NET
+    var context = new MLContext(seed: 1);
 
-// 3. Wybor algorytmu
-var trainer = context.Regression.Trainers.Sdca(labelColumnName: nameof(SalaryData.Salary));
+    // 2. Wczytanie danych z pliku CSV
+    IDataView dataView = context.Data.LoadFromTextFile<SalaryData>(dataPath, separatorChar: ',', hasHeader: true);
 
-// 4. Utworzenie schematu przetwarzania
-var pipeline = 
-    context.Transforms.Concatenate("Features", nameof(SalaryData.YearsExperience))
-    .Append(trainer);
-
-// 5. Trenowanie modelu na podstawie danych
-Console.WriteLine("Training...");
-var trainedModel = pipeline.Fit(trainingData);
-
-Console.WriteLine("Done.");
-
-// Ocena modelu na podstawie danych testowych
-var predictions = trainedModel.Transform(testData);
-
-var metrics = context.Regression.Evaluate(predictions, labelColumnName: nameof(SalaryData.Salary));
-
-// Wyswietlenie metryk
-Console.WriteLine($"Wspolczynnik determinacji (R^2): {metrics.RSquared}");
-
-// 6. Przykładowane dane do predykcji
-
-var sampleData = new SalaryData { YearsExperience = 5.5f };
-
-// 7. Tworzymy funkcje predykcji 
-var predictionFunction = context.Model.CreatePredictionEngine<SalaryData, SalaryPrediction>(trainedModel);
-
-// 8. Wywołanie predykcji
-var prediction = predictionFunction.Predict(sampleData);
-
-// 9 Wyswietlenie wyniku predykcji
-Console.WriteLine($"Przewidywana pensja dla {sampleData.YearsExperience} lat doswiadczenia wynosi {prediction.Salary}");
+    var dataSplit = context.Data.TrainTestSplit(dataView, testFraction: 0.2);
+    var trainingData = dataSplit.TrainSet;
+    var testData = dataSplit.TestSet;
 
 
-var prediction2 = predictionFunction.Predict(sampleData);
+    // 3. Wybor algorytmu
+    var trainer = context.Regression.Trainers.Sdca(labelColumnName: nameof(SalaryData.Salary));
 
-// 9 Wyswietlenie wyniku predykcji
-Console.WriteLine($"Przewidywana pensja dla {sampleData.YearsExperience} lat doswiadczenia wynosi {prediction2.Salary}");
+    // 4. Utworzenie schematu przetwarzania
+    var pipeline =
+        context.Transforms.Concatenate("Features", nameof(SalaryData.YearsExperience))
+        .Append(trainer);
+
+    // 5. Trenowanie modelu na podstawie danych
+    Console.WriteLine("Training...");
+    var trainedModel = pipeline.Fit(trainingData);
+    Console.WriteLine("Done.");
+
+    // Ocena modelu na podstawie danych testowych
+    var predictions = trainedModel.Transform(testData);
+
+    var metrics = context.Regression.Evaluate(predictions, labelColumnName: nameof(SalaryData.Salary));
+
+    // Wyswietlenie metryk
+    Console.WriteLine($"Wspolczynnik determinacji (R^2): {metrics.RSquared}");
+
+    // Zapisanie wytrenowanego modelu do pliku
+    context.Model.Save(trainedModel, trainingData.Schema, "predicate-salary-model.zip");
+}
+
+static void UseTrainedModel(float yearsExperience)
+{
+    // 1. Tworzymy kontekst 
+    var context = new MLContext();
+
+    // 2. Ladujemy model
+    var trainedModel = context.Model.Load("predicate-salary-model.zip", out _);
+
+    // 6. Przykładowane dane do predykcji
+
+    var sampleData = new SalaryData { YearsExperience = yearsExperience };
+
+    // 7. Tworzymy funkcje predykcji 
+    var predictionFunction = context.Model.CreatePredictionEngine<SalaryData, SalaryPrediction>(trainedModel);
+
+    // 8. Wywołanie predykcji
+    var prediction = predictionFunction.Predict(sampleData);
+
+    // 9 Wyswietlenie wyniku predykcji
+    Console.WriteLine($"Przewidywana pensja dla {sampleData.YearsExperience} lat doswiadczenia wynosi {prediction.Salary}");
+
+
+    var prediction2 = predictionFunction.Predict(sampleData);
+
+    // 9 Wyswietlenie wyniku predykcji
+    Console.WriteLine($"Przewidywana pensja dla {sampleData.YearsExperience} lat doswiadczenia wynosi {prediction2.Salary}");
+}
